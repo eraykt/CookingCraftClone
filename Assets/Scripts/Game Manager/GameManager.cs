3f6a50d3114753ee553fbac4cbc493362cdfe86d
eray <alieraykt@gmail.com>
2022-06-08 19:51:53 +0300

    game manager and basic world design

---
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+
+    private void Awake()
+    {
+        Singleton();
+    }
+
+    private void Singleton()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+
+        else
+            Destroy(this.gameObject);        
+    }
+}
