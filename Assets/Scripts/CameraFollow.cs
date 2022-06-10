38406945a79aad7b0d435e74df0a6ba8daa313e9
eray <alieraykt@gmail.com>
2022-06-10 14:07:25 +0300

    Revert "Revert "playerControl script""
    This reverts commit d546545967731dd3d9c82d18404b4bb7905f4ecf.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]private Transform target;
+    [SerializeField]private Vector3 offset;
+    [SerializeField]private float chaseSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!target)
+        {
+            target = GameObject.FindObjectOfType<PlayerControl>().transform;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position + offset, chaseSpeed * Time.deltaTime);
+    }
+}
