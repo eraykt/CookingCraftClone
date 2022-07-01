7e6a05499306aa4ea68013df58756bfd03d7f024
Caglar MUTLU <caglarmutlulu@gmail.com>
2022-07-01 15:23:44 +0300

    a

--- Assets/Scripts/Animations/DoorControl.cs
+++ Assets/Scripts/Animations/DoorControl.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class DoorControl : MonoBehaviour
 {
-<<<<<<< HEAD
-    
-    Animator anim;
-=======
-    //public Collider Kitchen, Saloon;
-    //public GameObject RightDoor, LeftDoor;
-    public Animator anim;
->>>>>>> 340f3d93cca95ad8656c77d3255b5beacad9154f
+
+    private Animator anim;
+
     [SerializeField]private bool Kitchen;
     // Start is called before the first frame update
     void Start()
