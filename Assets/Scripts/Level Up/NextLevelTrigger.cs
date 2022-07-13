31334c2d6b50a604c84fb11b3344bfae9e30af49
eray <alieraykt@gmail.com>
2022-07-13 15:47:22 +0300

    leveller birbirine bağlandı

--- Assets/Scripts/Level Up/NextLevelTrigger.cs
+++ Assets/Scripts/Level Up/NextLevelTrigger.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class NextLevelTrigger : MonoBehaviour
 {
+    public int level;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // sonraki level
+            SceneManager.LoadScene(level);
         }
     }
 }
